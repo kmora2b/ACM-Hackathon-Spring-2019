f95c21e47676355738657a0115c744b5b7f83452
Joseph Gutierrez <jgutierrez2199@gmail.com>
2019-04-27 17:53:26 -0600

    Add Script for Feed.

--- index.php
+++ index.php
@@ -1,35 +1,22 @@
 <?php
     include("base.php");
 ?>
-
-<div class = "feed">
-        <div class = "feedEntry">
-            <img class = "feedEntryPic" src = "goodworksep.png">
-            <h3 class = "feedEntryTitle"><strong>Name</strong></h3>
-            <h4 class = "feedEntryTimeDate"><strong>Date - Location</strong></h4>
-            <p class = "feedEntryText">Text description of the things<p>
-        </div>
-        <div class = "feedEntry">
-            <img class = "feedEntryPic" src = "goodworksep.png">
-            <h3 class = "feedEntryTitle"><strong>Name</strong></h3>
-            <h4 class = "feedEntryTimeDate"><strong>Date - Location</strong></h4>
-            <p class = "feedEntryText">Text description of the things<p>
-        </div>
-        <div class = "feedEntry">
-            <img class = "feedEntryPic" src = "goodworksep.png">
-            <h3 class = "feedEntryTitle"><strong>Name</strong></h3>
-            <h4 class = "feedEntryTimeDate"><strong>Date - Location</strong></h4>
-            <p class = "feedEntryText">Text description of the things<p>
-        </div>
-        <div class = "feedEntry">
-            <img class = "feedEntryPic" src = "goodworksep.png">
-            <h3 class = "feedEntryTitle"><strong>Name</strong></h3>
-            <h4 class = "feedEntryTimeDate"><strong>Date - Location</strong></h4>
-            <p class = "feedEntryText">Text description of the things<p>
-        </div>
-</div>
-
-May 11th, 2019 Franklin Mountains Exploration
-March 6th, 2019 Animal Shelter Volunteer Event
-June 3rd, 2019 El Dorado High School Band Fundraiser
-June 5th, 2019 UTEP Alumni Gathering
+<?php 
+     $dummydata = array(array("Images\badges\bird.png","Title Of Event","Date Of Event", "Location Of Event", "This is text describing the event in great lengthy detail1."), 
+                    array("Images\badges\bluestar.png","Title Of Event","Date Of Event", "Location Of Event", "This is text describing the event in great lengthy detail2."), 
+                    array("Images\badges\goldstar.png","Title Of Event","Date Of Event", "Location Of Event", "This is text describing the event in great lengthy detail3."), 
+                    array("Images\badges\hammer.png","Title Of Event","Date Of Event", "Location Of Event", "This is text describing the event in great lengthy detail4."), 
+                    array("Images\badges\\tree.png","Title Of Event","Date Of Event", "Location Of Event", "This is text describing the event in great lengthy detail5."),);
+                    
+    echo "<div class = \"feed\">";
+    for ($i=0; $i < 5; $i++){ 
+        
+    echo "   <div class = \"feedEntry\">";
+    echo "       <img class = \"feedEntryPic\" src = \"".$dummydata[$i][0]."\">";
+    echo "       <h3 class = \"feedEntryTitle\"><strong>".$dummydata[$i][1]."</strong></h3>";
+    echo "       <h4 class = \"feedEntryTimeDate\"><strong>".$dummydata[$i][2]."<br>".$dummydata[$i][3]."</strong></h4>";
+    echo "       <p class = \"feedEntryText\">".$dummydata[$i][4]."<p>";
+    echo "   </div>";
+    }
+           
+?>
