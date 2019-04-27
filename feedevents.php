9c6efe5cc9b6fde54f310f6d5dd9ca975d14d805
Kim <kmora2b@gmail.com>
2019-04-27 15:28:45 -0600

    Update feedevents.php

--- feedevents.php
+++ feedevents.php
@@ -1,6 +1,6 @@
 <?php
     include("base.php");
-?>
+?> <
 	<div class = "minutesSection">
         <h1 class = "mainTitle"> Events </h1>
         <div class = "divider"></div>
@@ -16,12 +16,22 @@
         <h3 class = "file"><img src = "Images/Agenda/doc.jpg" class = "docPic">
             <a href = "Documents/6-5-18 TWNA Minutes (General Meeting).pdf" target = "_blank">
             June 5th, 2019 UTEP Alumni Gathering </a></h3>
+		</div>
 		<?php 
-		$events = array("Franklin Mountains Exploration", "Animal Shelter Volunteer Event", "El Dorado High School Band Fundraiser", "UTEP Alumni Gathering");
-		foreach ($events as $e){
-			echo "<h3 class = 'file'> <img src = 'Images/Agenda/doc.jpg' class = 'docPic'>".$e."</h3>";
-		}
+		$events = array("Franklin Mountains Exploration", "Trans Mountain Junction","2019-04-27");
+		
+			echo "<div class = 'event'> 
+			<h2>
+				<img src = 'Images/Agenda/doc.jpg' class = 'docPic'>".$events[0]."</h2> 
+				<h3>Location: ".$events[1]."</h3>
+				<h3>Date: ".$events[2]."</h3>
+			<button>Join</button> 
+			
+			</div>";
+			
+		
+		?>
 	
-    </div>
+  
 	
 	
