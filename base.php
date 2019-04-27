0baa3f858a2d0e980ea66765c310956aae0a4b65
Joseph Gutierrez <jgutierrez2199@gmail.com>
2019-04-27 15:28:22 -0600

    More cleaning up.

--- base.php
+++ base.php
@@ -12,13 +12,10 @@
         <nav>
             <div class = "topNav" id = "myTopNav">
                 <h3 class = "topNavItem"><strong><a href = "index.php">Home</a></strong></h3>
-                <!---<h3 class = "topNavItem"><strong><a href = "who-we-are.php">Who We Are</a></strong></h3>--->
-                <h3 class = "topNavItem"><strong><a href = "calendar.php">Community Calendar</a></strong></h3> 
-                <h3 class = "topNavItem"><strong><a href = "agendas.php">Find Events</a></strong></h3>
-                <!--<h3 class = "topNavItem"><strong><a href = "meet-the-officers.php">Meet The Officers</a></strong></h3>-->
-                <h3 class = "topNavItem"><strong><a href = "gallery.php">Gallery</a></strong></h3>
-                <!--<h3 class = "topNavItem"><strong><a href = "contact-us.php">Contact Us</a></strong></h3>-->
-				<h3 class = "topNavItem"><strong><a href = "gallery.php">Profile</a></strong></h3>
+                <h3 class = "topNavItem"><strong><a href = "community-calendar.php">Community Calendar</a></strong></h3> 
+				<h3 class = "topNavItem"><strong><a href = "profile.php">Profile</a></strong></h3>
+                <h3 class = "topNavItem"><strong><a href = "volunteer-log.php">Volunteer Log</a></strong></h3>
+                <h3 class = "topNavItem"><strong><a href = "profile.php">Welcome!</a></strong></h3>
 
                 <h3 class = "topNavItem"><strong><a href = "#" class = "menuButton" onclick = "toggleNav()">&#x2630;</a></strong></h3>
 				
