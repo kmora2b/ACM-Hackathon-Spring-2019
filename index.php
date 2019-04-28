81bfb22eb5a58fb51cf82ab0ec28039a55f39b8c
Joseph Gutierrez <jgutierrez2199@gmail.com>
2019-04-28 09:40:02 -0600

    fix image pointers

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 ?>
 <?php 
      $dummydata = array(array("Images\IMG_7767.jpg","Food Drive","April 29th, 2019", "Eastwood High School", "Volunteers needs for food drive."), 
-                    array("Images\4e931b10a084e.image.jpg","Charity Drive","April 30th, 2019", "Album Park", "Help needed at Album Park for annual charity drive."), 
-                    array("Images\20180515_140400.jpg","Community Night","April 30th, 2019", "Esperanza Acosta Moreno Library", "Volunteers needed for community night."), 
-                    array("Images\franklin-732x360.jpg","Franklin Mountains Event","May 20th, 2019", "Franklin Mountains State Park", "Volunteered needed to help clean litter and raise awareness."),);
+                    array("Images\\4e931b10a084e.image.jpg","Charity Drive","April 30th, 2019", "Album Park", "Help needed at Album Park for annual charity drive."), 
+                    array("Images\community.jpg","Community Night","April 30th, 2019", "Esperanza Acosta Moreno Library", "Volunteers needed for community night."), 
+                    array("Images\\franklin-732x360.jpg","Franklin Mountains Event","May 20th, 2019", "Franklin Mountains State Park", "Volunteered needed to help clean litter and raise awareness."),);
                     
     echo "<div class = \"feed\">";
     for ($i=0; $i < 4; $i++){ 
