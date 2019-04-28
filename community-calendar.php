20291b3773767d53543a2aa42dd31d9b4d492de6
Kim <kmora2b@gmail.com>
2019-04-27 21:36:49 -0600

    Update community-calendar.php

--- community-calendar.php
+++ community-calendar.php
@@ -2,4 +2,4 @@
     include("base.php");
 ?>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%2333ccff&amp;src=goodworksacmhackathon%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FDenver" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe class = 'calendar'src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%2333ccff&amp;src=goodworksacmhackathon%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FDenver" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
