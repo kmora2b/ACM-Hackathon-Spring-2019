643d785b912ef280491307f4e5f3e3774542ddd5
Kim <kmora2b@gmail.com>
2019-04-27 22:32:28 -0600

    Volunteer Log and Stylesheet updated

---
+++ volunteer-log.php
@@ -0,0 +1,34 @@
+<?php
+    include("base.php"); 
+?>
+
+
+<table class='log'>
+	<tr class='log-header'>
+		
+		<th>TITLE</th>
+		<th>DATE</th>
+		<th>LOCATION</th>
+		<th># OF HOURS</th>
+	</tr>
+	
+	<tr>
+		<th>TITLE</th>
+		<th>DATE</th>
+		<th>LOCATION</th>
+		<th># OF HOURS</th>
+	</tr>
+	<?php
+		$l = array("Franklin Mountains", "2018-05-20", "Trans Mountains","Explorers");
+		echo"<tr>
+			<th>".$l[0]."</th>
+			<th>".$l[1]."</th>
+			<th>".$l[2]."</th>
+			<th>".$l[3]."</th>
+		
+		</tr>";
+	?>
+	
+</table>
+
+
